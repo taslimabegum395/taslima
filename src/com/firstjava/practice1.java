12e0bf91abf77ae78c3104c44fdda1384f88231b
gtas6 <gtas6@DESKTOP-16EPLLK>
2022-08-09 17:32:31 -0400

    first commit

---
+++ src/com/firstjava/practice1.java
@@ -0,0 +1,20 @@
+
+package com.firstjava;
+
+public class practice1 {
+
+	int a=10;
+	int b=20;
+	c= a+b;
+	d=a-b;
+	f=35;
+	Sys
+	
+	
+	 
+	
+	
+	}
+
+}
+
---
+++ src/com/javalearn/learnjava.java
@@ -0,0 +1,47 @@
+package com.javalearn;
+
+public class learnjava {
+	int a=50;
+	int b=20;
+	int c=10;
+	int d=a+b;
+	int e=a-b;
+	int f=a*b;
+	public void learn() {
+		System.out.println(d);
+		System.out.println(e);
+	}
+			
+public void learn1() {
+	System.out.println(f);
+	
+	
+}	
+ public void learn2() {
+	 System.out.println("c");
+	 
+ }			
+			
+			
+
+public static void main(String[]args) {
+	//className objectname=new className();
+	learnjava r=new learnjava();
+	r.learn1();
+	r.learn2();
+	r.learn();
+
+}	
+}
+
+
+
+
+			
+
+	
+	
+	
+
+	
+
